9ab05d9580ac5d0c48730c4e512044fd1814bfed
gouthamnandula <gouthamnandula@gmail.com>
2026-06-21 21:16:06 +0000

    chore: format code and improve consistency across configuration files

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
+#!/bin/sh
 set -e
 
 NAME="kubernetes-demo-api"
 USERNAME="gouthamnandula"
-Image = "$USERNAME/$NAME:latest"
+IMAGE="$USERNAME/$NAME:latest"
 
 echo "Building Docker Image"
-docker build -t $Image .
+docker build -t "$IMAGE" .
 
 echo "Pushing Docker Image to Docker Hub"
-docker push $Image
+docker push "$IMAGE"
 
 echo "Deploying to Kubernetes"
 kubectl apply -f k8s/deployment.yaml
@@ -20,5 +21,7 @@ kubectl get pods
 echo "Getting services"
 kubectl get services
 
+SERVICE_NAME="devops-kubernetes-api-service"
+
 echo "Fetching the main service"
-kubectl get service $NAME-service
+kubectl get service "$SERVICE_NAME"
